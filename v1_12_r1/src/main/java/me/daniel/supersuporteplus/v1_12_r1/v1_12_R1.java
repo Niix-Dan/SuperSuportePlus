1498d4c1f4c7624a9a715b332bacefbaca831786
Daniel-code15 <gatocraftbr@gmail.com>
2021-05-28 16:24:53 -0300

    First Commit

---
+++ v1_12_r1/src/main/java/me/daniel/supersuporteplus/v1_12_r1/v1_12_R1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.daniel.supersuporteplus.v1_12_r1;
+
+/**
+ *
+ * @author SrNox_
+ */
+public class v1_12_R1 extends nms {
+    
+}
